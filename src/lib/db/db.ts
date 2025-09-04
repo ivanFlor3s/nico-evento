4be539ac0e1e1b329e707a27c80915d02c93a317
Ivan Flores <ifloresmanc96@gmail.com>
2025-09-04 11:04:50 -0300

    create order in db

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -39,3 +39,42 @@ export const getOfferLandingById = (id: string) => {
         }
     })
 }
+
+export interface OrderData {
+    buyerName: string;
+    buyerLastName: string;
+    buyerEmail: string;
+    payMethod: string; // "mercadopago" | "cash"
+    total: number;
+    status: string; // "pending" | "paid" | "cancelled"
+    paymentId?: string; // ID del pago en MercadoPago
+    preferenceId?: string; // ID de la preferencia de MercadoPago        
+}
+
+export interface OrderItemData {
+    productId?: string;
+    comboId?: string;
+    quantity: number;
+    price: number;
+    subtotal: number;
+}
+
+export const createOrder = (orderData: OrderData, orderItems: OrderItemData[]) => {
+    console.log('📦 Creando orden con datos:', orderItems.map(x => x.comboId));
+    return prisma.order.create({
+        data: {
+            ...orderData,
+            orderItems: {
+                create: orderItems
+            }
+        },
+        include: {
+            orderItems: {
+                include: {
+                    product: true,
+                    combo: true
+                }
+            }
+        }
+    })
+}
