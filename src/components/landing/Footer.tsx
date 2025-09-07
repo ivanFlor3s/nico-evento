7a55dd00b0ca8c260bb72326dfbfc7b16f69bfed
Ivan Flores <ifloresmanc96@gmail.com>
2025-09-07 19:57:44 -0300

    fix: event date

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer: React.FC = () => {
     return (
         <footer id="footer" className="">
             <div className="container mx-auto px-4 text-center">
-                <p className="mb-4">© 2024 Evento Peña. Todos los derechos reservados.</p>
+                <p className="mb-4">© 2025 Evento Peña. Todos los derechos reservados.</p>
             </div>
         </footer>
     );
